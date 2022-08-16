6d701513b4ab6b3e9deeaba19b74056e39b9bd9f
Ivan Lutsenko <evanlt187@gmail.com>
2022-08-16 21:52:06 +0300

    put abstactions into application

--- Core/Domain/Models/User.cs
+++ Core/Domain/Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace Domain.Models
+﻿using Domain.Enums;
+
+namespace Domain.Models
 {
     public class User
     {
@@ -7,7 +9,7 @@
         public string Surname { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
-        public string Role { get; set; }
+        public UserRole Role { get; set; }
         public ICollection<Post> Posts { get; set; }
     }
 }
