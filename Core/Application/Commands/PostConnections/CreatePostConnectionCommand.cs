593ed466e995e74955db30dbf1ba9fe447e8b9ba
SunnyPiase <petya5671@gmail.com>
2022-08-18 12:03:31 +0300

    added CreatePostConnection+Handler

---
+++ Core/Application/Commands/PostConnections/CreatePostConnectionCommand.cs
@@ -0,0 +1,39 @@
+﻿using Application.UnitOfWorks;
+using Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.PostConnections
+{
+    public class CreatePostConnectionCommand : IRequest<PostConnection>
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public int VolunteerPostId { get; set; }
+        public int NeedfulPostId { get; set; }
+    }
+    public class CreatePostHandler : IRequestHandler<CreatePostConnectionCommand, PostConnection>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreatePostHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<PostConnection> Handle(CreatePostConnectionCommand request, CancellationToken cancellationToken)
+        {
+            return new PostConnection()
+            {
+                Title = request.Title,
+                Message = request.Message,
+                VolunteerPost = await _unitOfWork.Posts.GetById(request.VolunteerPostId),
+                NeedfulPost = await _unitOfWork.Posts.GetById(request.NeedfulPostId),
+            };
+        }
+        
+    }
+}
